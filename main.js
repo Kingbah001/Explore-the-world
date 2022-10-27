52ecc3be9f3df50cf8c61a9b1966ae4869746bfe
Kingbah001 <114989677+Kingbah001@users.noreply.github.com>
2022-10-27 02:05:53 -0700

    complete of the hamburger section

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 
 
-console.log()
+// console.log(document)
+
+
+
+// console.log(hamburgerdiv.)
+
+
+var hamburgerdiv = document.querySelector(".hamburger")
+var mobileLinks = document.querySelector(".moblie-links-holder")
+var mybackdrop = document.querySelector(".backdrop")
+
+function dosomething() {
+hamburgerdiv.classList.toggle("showburger")
+mobileLinks.classList.toggle("show-moblie-links-holder")
+mybackdrop.classList.toggle("show-backdrop")
+}
